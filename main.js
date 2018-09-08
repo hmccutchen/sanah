be6be478aae632371e4b222f20c11cf917e65178
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2018-09-08 01:30:07 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,75 @@
+var tag = document.createElement('script');
+
+      tag.src = "https://www.youtube.com/iframe_api";
+      var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+      // 3. This function creates an <iframe> (and YouTube player)
+      //    after the API code downloads.
+      var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '0',
+          width: '1280',
+          videoId: 'E2P4ZmNIk1o',
+          disablekb: 1,
+
+          theme: 'light',
+
+          playerVars: { 'controls': 0,
+                        'modestbranding': 1,
+                        'rel': 0,
+                        'playsinline': 1,
+                        'showinfo': 0
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+      // 4. The API will call this function when the video player is ready.
+      function onPlayerReady(event) {
+        event.target.playVideo();
+        event.target.setVolume(0);
+
+      }
+
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+          setTimeout(stopVideo, 49500);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+
+
+
+
+
+document.addEventListener("scroll", function(){
+//if I add more card classes this function will need to change
+var card = document.getElementsByClassName("card");
+card[0].style.backgroundColor = "#f4f5f7";
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
